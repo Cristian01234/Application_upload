14e9df6c6f6ba346131784f5f036b55c7c9cefa7
40751 <cristyardy2@gmail.com>
2022-11-21 16:45:44 +0200

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+
+    const [users, setUsers]=useState([])
+
+    useEffect(()=>{
+       loadUsers();
+    }, []);
+
+    const loadUsers= async () =>{
+        const result = await axios.get("http://localhost:8081/users");
+       setUsers(result.data)
+    }
+    
+
+
+  return (
+   <div className="container">
+   <div className="py-4">
+   <table className="table border shadow">
+   <thead>
+     <tr>
+       <th scope="col">#</th>
+       <th scope="col">Name</th>
+       <th scope="col">Username</th>
+       <th scope="col">Email</th>
+       <th scope="col">Actions</th>
+     </tr>
+   </thead>
+   <tbody>
+    {
+      users.map((users, index) =>(
+        <tr>
+        <th scope="row" key={index}>{index+1}</th>
+        <td>{users.name}</td>
+        <td>{users.username}</td>
+        <td>{users.email}</td>
+        <td>
+        <button className='btn btn-primary mx-2'>View</button>
+        <Link className='btn btn-outline-primary mx-2'
+        
+        to={`/edituser/${users.id}`}
+        >Edit</Link>
+        <button className='btn btn-danger mx-2'>Delete</button>
+        </td>
+      </tr>
+      ))
+    }
+
+
+
+    
+  
+   </tbody>
+ </table>
+   </div>
+   </div>
+  )
+}
